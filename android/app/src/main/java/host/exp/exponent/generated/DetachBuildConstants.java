4c543f63745e4caf11ccd28698a808e134fb0443
Ernesto <ernestoivan22@hotmail.com>
2019-08-23 16:28:55 -0600

    pspdfkit 1.24.0 android issue: Program type already present: com.facebook.device.yearclass.BuildConfig

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "expd75774c41a2a4f6a9de71739a6378a62://192.168.0.35:19000";
+
+}
