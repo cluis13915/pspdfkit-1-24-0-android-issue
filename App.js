89bbaf2f53f32006d1c18b94693d7ea9b4fb5b3a
cluis <cesarluis13915@gmail.com>
2019-10-03 18:08:08 -0600

    Create PDF file with 'react-native-pdf-lib' and open it with PSPDFKit.present().

--- App.js
+++ App.js
@@ -1,12 +1,90 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  NativeModules,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import PDFLib, { PDFDocument, PDFPage } from 'react-native-pdf-lib';
+
+let PSPDFKit = NativeModules.PSPDFKit;
+
+const CONFIGURATION = {
+  scrollContinuously: false,
+  showPageNumberOverlay: true,
+  pageScrollDirection: "vertical",
+  showShareAction: false,
+  showPrintAction: false,
+};
+
+
+async function generatePdfWithPdfLib(state = {}) {
+  const page1 = PDFPage
+    .create()
+    .setMediaBox(200, 300)
+    .drawText('PDF test file.', {
+      x: 50,
+      y: 140,
+      color: '#000000',
+    });
+
+  const docsDir = await PDFLib.getDocumentsDirectory();
+  const pdfPath = `${docsDir}/sample.pdf`;
+
+  PDFDocument
+    .create(pdfPath)
+    .addPages(page1)
+    .write()
+    .then(path => {
+      console.log('PDF created at: ' + path);
+      state.document = path;
+    });
+}
+
+
+export default class App extends React.Component<{}> {
+  constructor(props) {
+    super(props);
+
+    this.state = { document: null };
+
+    this._onOpenPdf = this._onOpenPdf.bind(this);
+    this._onGeneratePdf =  this._onGeneratePdf.bind(this);
+  }
+
+  _onGeneratePdf(state) {
+    generatePdfWithPdfLib(state);
+  }
+
+  _onOpenPdf() {
+    const { document } = this.state;
+
+    if (!document) {
+      return;
+    }
+
+    try {
+      PSPDFKit.present(document, CONFIGURATION);
+    } catch (err) {
+      console.warn(err);
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>PSPDFKit {PSPDFKit.versionString}</Text>
+        <TouchableOpacity onPress={() => this._onGeneratePdf(this.state)}>
+          <Text style={styles.text}>Create Document</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this._onOpenPdf}>
+          <Text style={styles.text}>Open Document</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,4 +94,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  }
 });
--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "expd75774c41a2a4f6a9de71739a6378a62://192.168.0.35:19000";
+  public static final String DEVELOPMENT_URL = "expd75774c41a2a4f6a9de71739a6378a62://192.168.1.151:19000";
 
 }
